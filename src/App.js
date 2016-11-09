ee07c67b9cc8455c3f006118f14d6a48f3d6f796
dannycrews <dancrews@me.com>
2016-11-09 13:49:17 -0500

    Set initial state and onChange handler. Define MarkdownPane props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 import './App.scss';
+import Marked from 'marked';
 
 import MarkdownPane from './MarkdownPane';
 import PreviewPane from './PreviewPane';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.onMarkdownChange = this.onMarkdownChange.bind(this);
+    this.state = {
+      markdownText: 'default input'
+    }
+  }
+
+  onMarkdownChange(evt) {
+    console.log('event = ' + evt.target);
+    const { value } = evt.target;
+    this.setState({
+      markdownText: value
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -12,7 +30,7 @@ class App extends Component {
           <h1>Markdown Previewer</h1>
         </div>
         <div className='MarkdownPane'>
-          <MarkdownPane />
+          <MarkdownPane markdownText={this.state.markdownText} onChange={this.onMarkdownChange} />
         </div>
         <div className='PreviewPane'>
           <PreviewPane />
@@ -22,5 +40,7 @@ class App extends Component {
     );
   }
 }
+
+
 
 export default App;
--- src/MarkdownPane.js
+++ src/MarkdownPane.js
@@ -6,10 +6,20 @@ class MarkdownPane extends Component {
     return (
       <div className="markdown-container">
         <h2>Markdown Pane</h2>
-        <textarea className="textentry" />
+        <textarea
+          className="textentry"
+          onChange={ this.props.onChange }
+          value={this.props.markdownText}
+
+         />
       </div>
     );
   }
 }
+
+MarkdownPane.propTypes = {
+  onChange: React.PropTypes.func.isRequired,
+  markdownText: React.PropTypes.string.isRequired
+};
 
 export default MarkdownPane;
